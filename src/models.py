4f59ceb556b03017aae0a78960d263f4d2a3bd4c
ArausyCorzo <arausyncc@gmail.com>
2022-01-10 02:23:25 +0000

    hello

--- src/models.py
+++ src/models.py
@@ -4,12 +4,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    favorites = db.relationship("Favorites", backref="user", uselist=True)
 
     def serialize(self):
         return {
@@ -17,3 +15,14 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    url = db.Column(db.String(150), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    _table_args_ = (db.UniqueConstraint(
+        "user_id",
+        "url",
+        name = "unique_favorite_user"
+    ),)
